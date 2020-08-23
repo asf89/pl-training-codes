495a88819ba380457d08cc4d71d4ddec3bbce5c4
André Ferreira <asf@cesar.org.br>
2020-08-23 16:44:24 -0300

    add package example

--- Common_Lisp/PackageExample/arith-ops/src/main.lisp
+++ Common_Lisp/PackageExample/arith-ops/src/main.lisp
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 (defpackage :arith-ops
   (:use :cl :math-ops)  
 )
 (in-package :arith-ops)
-=======
-(defpackage #:arith-ops
-  (:use #:cl #:math-ops)  
-)
-(in-package #:arith-ops)
->>>>>>> fb3dca9ff99e0a476bf4c8d374382efd98c6a946
 
 (format t "Multiplication: ~D~%" (math-ops:mult 3 5))
 ;; blah blah blah.
